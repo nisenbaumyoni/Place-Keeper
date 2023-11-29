9006b5bc1397d96ec2487acde75080dfe3c716f7
Yoni Nisenbaum <yoni.nisenbaum@gmail.com>
2023-11-29 16:43:42 +0200

    export data

--- js/placeController.js
+++ js/placeController.js
@@ -1,80 +1,59 @@
-import { placeService } from "./services/placeService.js"
+import { placeService } from "./services/placeService.js";
 
-window.initMap = initMap
-window.initPlaces = initPlaces
-window.onRemovePlace = onRemovePlace
-window.onZoomPlace = onZoomPlace
-window.onAddPlace = onAddPlace
+window.initMap = initMap;
+window.initPlaces = initPlaces;
+window.onRemovePlace = onRemovePlace;
+window.onZoomPlace = onZoomPlace;
+window.onAddPlace = onAddPlace;
+window.onExportPlaces = onExportPlaces;
 
-function initMap(name = 'Eilat', lat = 29.55724, lng = 34.95294) {
-  const location = {lat : lat , lng : lng}
-  var elMap = document.querySelector(".map")
+function initMap(name = "Eilat", lat = 29.55724, lng = 34.95294) {
+  const location = { lat: lat, lng: lng };
+  var elMap = document.querySelector(".map");
   var options = {
     zoom: 8,
-    center: location
-}
+    center: location,
+  };
 
-  var map = new google.maps.Map(elMap, options)
+  var map = new google.maps.Map(elMap, options);
 
   var marker = new google.maps.Marker({
     position: location,
     map,
-    title: name
-  })
+    title: name,
+  });
 
+  // Add Listener for panning locations on the map
   map.addListener("click", (event) => {
-    var latitude = event.latLng.lat()
-    var longitude = event.latLng.lng()
-    var name = prompt("enter location name")
-    if(name !=="" && name!==null){
-        placeService.addPlace(name, latitude, longitude);
-        renderPlaces()
-        initMap(name,latitude,longitude)} 
-  })
+    var latitude = event.latLng.lat();
+    var longitude = event.latLng.lng();
+    var name = prompt("enter location name");
+    if (name !== "" && name !== null) {
+      placeService.addPlace(name, latitude, longitude);
+      _renderPlaces();
+      initMap(name, latitude, longitude);
+    }
+  });
 
+  // Create the DIV to hold the control.
+  const centerControlDiv = document.createElement("div");
+  // Create the control.
+  const centerControl = createCenterControl(map);
+
+  // Append the control to the DIV.
+  centerControlDiv.appendChild(centerControl);
+  map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(centerControlDiv);
 }
 
 function initPlaces() {
-  renderPlaces()
+  _renderPlaces();
 }
 
-async function renderPlaces() {
-  let places = await placeService.getPlaces()
-  console.log("places", places)
-  let strHtmls = `
-        <form id="add-place" onsubmit="onAddPlace(event)">
-            <div class="row">
-            <div class="col-25">
-                <label for="place-name">Name</label>
-            </div>
-            <div class="col-75">
-                <input type="text" id="placeName" name="placeName" class="placeName">
-            </div>
-            </div>
-            <div class="row">
-            <div class="col-25">
-                <label for="lat">Latitude</label>
-            </div>
-            <div class="col-75">
-                <input type="number" id="lat" name="lat" min="0" step="0.0000001" class="lat-input">
-            </div>
-            </div>
-            <div class="row">
-            <div class="col-25">
-                <label for="lng">Longitude</label>
-            </div>
-            <div class="col-75">
-                <input type="number" id="lng" name="lng" min="0" step="0.01" class="lng-input">
-            </div>
-            </div>
-            
-            <div class="row">
-            <input type="submit" value="Add place">
-            </div>
+async function _renderPlaces() {
+  let places = await placeService.getPlaces();
+  let strHtmls = `<button class="export-button" onClick="onExportPlaces()">Export Places</button>
+    <p></p>`;
 
-            <br><br>
-        </form>
-    `;
   strHtmls += places
     .map(
       (place) => `
@@ -96,27 +75,93 @@ function onAddPlace(ev) {
   const addedPlace = Object.fromEntries(new FormData(ev.target));
   console.log("addedPlace, ", addedPlace);
   placeService.addPlace(addedPlace.placeName, addedPlace.lat, addedPlace.lng);
-  renderPlaces();
+  _renderPlaces();
 }
 
-function onAddPlaceOnMap(name,lat,lng) {
-    placeService.addPlace(name, lat, lng);
-    renderPlaces();
-  }
+function onAddPlaceOnMap(name, lat, lng) {
+  placeService.addPlace(name, lat, lng);
+  _renderPlaces();
+}
 
 async function onRemovePlace(placeId) {
   console.log("onRemovePlace!, ", placeId);
   const result = await placeService.removePlace(placeId);
-  renderPlaces();
+  _renderPlaces();
 }
 
 async function onZoomPlace(placeId) {
   try {
     const place = await placeService.getPlaceById(placeId);
-    initMap(place.name,place.lat,place.lng)
-
+    initMap(place.name, place.lat, place.lng);
   } catch (err) {
     console.log("Error", err);
     flashMsg("Cannot zoom place");
   }
+}
+
+function createCenterControl(map) {
+  const controlButton = document.createElement("button");
+
+  controlButton.style.backgroundColor = "#fff";
+  controlButton.style.border = "2px solid #fff";
+  controlButton.style.borderRadius = "3px";
+  controlButton.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+  controlButton.style.color = "rgb(25,25,25)";
+  controlButton.style.cursor = "pointer";
+  controlButton.style.fontFamily = "Roboto,Arial,sans-serif";
+  controlButton.style.fontSize = "16px";
+  controlButton.style.lineHeight = "38px";
+  controlButton.style.margin = "8px 10px 22px";
+  controlButton.style.padding = "0 5px";
+  controlButton.style.textAlign = "center";
+  controlButton.textContent = "🎯";
+
+  controlButton.title = "Click to center on your location";
+  controlButton.type = "button";
+
+  controlButton.addEventListener("click", () => {
+    try {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const currentLatitude = position.coords.latitude;
+        const currentLongitude = position.coords.longitude;
+        const name = "My Location!";
+
+        initMap(name, currentLatitude, currentLongitude);
+      });
+    } catch (err) {
+      console.log(
+        "Geolocation is not supported by this browser. error is",
+        err
+      );
+    }
+  });
+  return controlButton;
+}
+
+async function onExportPlaces() {
+  console.log("inside onExportPlaces");
+
+  let placesArray = await placeService.getPlaces();
+
+  _downloadPlacesAsCSV(placesArray);
+
+  console.log("placesArray", placesArray);
+}
+
+function _downloadPlacesAsCSV(places) {
+  let data = `id,name,lat,lng\n`;
+  for (let p of places) {
+    data += `${p.id},${p.name},${p.lat},${p.lng}\n`;
+  }
+
+  let csv_file = new Blob([data], { type: "text/csv" });
+  let tmp_link = URL.createObjectURL(csv_file);
+
+  let dlink = document.createElement("a");
+  dlink.download = "places.csv";
+  dlink.href = tmp_link;
+  dlink.click();
+
+  URL.revokeObjectURL(tmp_link); // Delete the tmp_link
+  dlink.remove();
 }
--- js/userSettingsController.js
+++ js/userSettingsController.js
@@ -1,7 +1,6 @@
 // TODO
 // 2. add custom control on the map
 // 3. when saving user prefs , change for all pages
-// 4. change "1" to userDb
 import {userService} from './services/userService.js'
 import {utilService} from './services/utilService.js';
 
@@ -17,27 +16,11 @@ function initUserSettings(){
     console.log("element is",element);
     console.log("element.value is",element.value);
     element.value = userPrefs.BackgroundColor;
-
-    //element.setAttribute()
-    //document.getElementById('.backgroundColorInputClass').value = element.value
-    // userPrefs.BackgroundColor;
 }
 
 function handleSubmit(ev){
     ev.preventDefault();
-    /*
-    console.log("initUserSettings ",ev);
-    let emailClass = document.querySelector(".email-input");
-    console.log(emailClass.value);
-    let emailId = document.querySelector("#email");
-    console.log(emailId.value);
-    let emailAttr = document.querySelector("[name=email]");
-    console.log(emailAttr.value);
-    */
-
     const userPrefs = Object.fromEntries(new FormData(ev.target));
     userPrefs.points=+userPrefs.points;
-    //console.log(userPrefs);
-
     userService.setUser(userPrefs);
 }
