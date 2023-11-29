f05c142c9c2fc67a95288ed0539cb9d0a99abc53
Yoni Nisenbaum <yoni.nisenbaum@gmail.com>
2023-11-29 18:24:59 +0200

    code cleanup

--- js/mainController.js
+++ js/mainController.js
@@ -1,30 +1,17 @@
-//import {placeService} from './services/placeService.js';
-//import {userService} from './services/userService.js';
-//import {utilService} from './services/utilService.js';
-// 
 import {utilService} from './services/utilService.js';
 
-
 window.initPlaceKeeper = initPlaceKeeper;
-
-
-
-
-
 
 function initPlaceKeeper() {
     console.log("initPlaceKeeper");
 
-    let userPrefs = utilService.loadFromStorage(1);
+    let userPrefs = utilService.loadFromStorage("userDB");
 
     const element = document.querySelector('#main');
     console.log("element is",element);
 
     element.style.backgroundColor = userPrefs.BackgroundColor;
 
-}
-
-function initUserSettings() {
 }
 
 function renderPlaces() {
